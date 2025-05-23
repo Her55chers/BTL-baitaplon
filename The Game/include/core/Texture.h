23468ed2f679a26ce61be98eb0659db360e05ca7
Her55chers <vulinh200223@gmail.com>
2025-05-23 08:34:36 +0700

    Final

--- The Game/include/core/Texture.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Game.h"
-
-class Texture
-{
-public:
-    static SDL_Texture* loadTexture(const char* file);
-    static void Draw(SDL_Texture* tex, SDL_Rect src, SDL_Rect dst);
-};
